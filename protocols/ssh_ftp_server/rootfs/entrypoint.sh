a05d6ddb8f3257b330a266b87abe740bec6fc69b
liranmatcu <l.ma@tcu.edu>
2019-02-13 22:50:52 -0600

    ftp and ssh worked on one container
    docker build -t ssh-ftp-server .
    docker run -d -p 22:22 ssh-ftp-server

--- protocols/ssh_ftp_server/rootfs/entrypoint.sh
+++ protocols/ssh_ftp_server/rootfs/entrypoint.sh
@@ -3,5 +3,22 @@
 # generate host keys if not present
 ssh-keygen -A
 
+# FTP parameters
+echo "pasv_max_port=$PASV_MAX" >> /etc/vsftpd/vsftpd.conf 
+echo "pasv_min_port=$PASV_MIN" >> /etc/vsftpd/vsftpd.conf 
+echo "pasv_address=$PASV_ADDRESS" >> /etc/vsftpd/vsftpd.conf 
+
+addgroup -g 433 -S $FTP_USER
+adduser -u 431 -D -G $FTP_USER -h /home/$FTP_USER -s /bin/false  $FTP_USER
+
+echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd
+chown $FTP_USER:$FTP_USER /home/$FTP_USER/ -R
+
+# Start the ftpd process
+/usr/sbin/vsftpd.sh &
+
+
+# Start the sshd process
 # do not detach (-D), log to stderr (-e), passthrough other arguments
 exec /usr/sbin/sshd -D -e "$@"
+
