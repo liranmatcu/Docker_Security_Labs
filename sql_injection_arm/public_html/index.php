106dff8b3d715fa9ca36a2bf5984d6513b511a24
Liran Ma <l.ma@tcu.edu>
2023-05-02 21:05:51 -0500

    Added SQL injection for M1/M2 Macs
    ARM chips

---
+++ sql_injection_arm/public_html/index.php
@@ -0,0 +1,46 @@
+<h1>Welcome to SQL Injection Demo Site</h1>
+
+<?php 
+function submit() {
+    $host = 'mysql';
+    $user = 'root';
+    $pass = 'password';
+    $dbname = 'injectionAttack';
+    $conn = new mysqli($host, $user, $pass, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+    $product = $_POST['product'];
+
+    #removes the last extra space/spaces in user input
+    // $product = rtrim($product);
+    
+    #used for removing characters used for sql injection
+    // $product = str_replace(array("(", ")" , ";" , "--"), '', $product);
+
+    $sql = "SELECT id, product_name, product_type, description, price  FROM products WHERE product_name LIKE '%$product%'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "<table><tr><th>ID</th><th>Name</th><th>Type</th><th>Description</th><th>Price</th></tr>";
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["id"]."</td><td>".$row["product_name"]."</td><td>".$row["product_type"]."</td><td>".$row["description"]."</td><td>".$row["price"]."</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+}
+if(array_key_exists('submit', $_POST)) {
+    submit();
+}
+?>
+
+<form method="post">
+    <input type="text" id="product" name="product">
+    <input type="submit" value="Submit" name="submit">
+</form>
